d876d871382156ee6575a22eb851ce20795b2806
MichaelRedant <donmichaelR@gmail.com>
2025-08-06 00:46:53 +0200

    shared

--- src/pages/SharedWorkspace.jsx
+++ src/pages/SharedWorkspace.jsx
@@ -6,10 +6,6 @@ import Button from '../components/Button';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-
-import { useParams, useNavigate } from 'react-router-dom';
-import Button from '../components/Button';
-
   const [data, setData] = useState(null);
   const [error, setError] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
